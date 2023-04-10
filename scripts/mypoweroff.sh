d1f670042d1844e4555d61bb98ffc8741e8178c1
Nigel James <nigel@james.gen.nz>
2023-04-10 19:16:55 +1200

    Backup triggered on 2023-04-10 19:16:55
    Klipper on commit: 83308a1
    
    Moonraker on commit: 31e589a
    
    Fluidd version: v1.23.4

--- scripts/mypoweroff.sh
+++ scripts/mypoweroff.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-delay=120
+delay=100
 log=~/printer_data/logs/klippy.log
 tasmota_device=192.168.2.25
 
 echo Issuing ${delay}ds delayed Tasmota power off for `hostname` to ${tasmota_device} | tee -a ${log}
 /usr/bin/curl --netrc-file /home/pi/printer_data/.myIOTpwd "http://${tasmota_device}/cm?cmnd=Backlog%3BDelay%20$delay%3BPower%20Off" &>> ${log}
-
-sleep 2
