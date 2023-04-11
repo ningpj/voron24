22eb5ff0739022674acedfde2a549bcd6043f759
Nigel James <nigel@james.gen.nz>
2023-04-11 12:14:51 +1200

    Backup triggered on 2023-04-11 12:14:51
     "klipper on commit: v0.9.1 83308a1"
    
     "moonraker on commit: v0.8.0 31e589a"
    
     "fluidd on commit: v1.23.4"
    
     "ERCF-Software-V3 on commit: v1.2.4 8b8a6ae"
    
     "KlipperScreen on commit: v1.0.1 a6bf805"

--- scripts/mygitbkup.sh
+++ scripts/mygitbkup.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
-set -x
 
+# File containing our github secret token held outside all git repos containing mygittoken=<token> 
 export $(grep -v '^#' ~/printer_data/.mygittoken | xargs -0)
 
+# GIT local repos installed on your printer. This is only to add version numbers references as messages for the commit 
 gitrepos=(~/klipper ~/moonraker ~/mainsail ~/fluidd ~/ERCF-Software-V3 ~/KlipperScreen)
+
+# Location of klipper configuration
 printer_config=~/printer_data/config
+
+# Directory within your klipper configuration to hold a backup of the moonraker database 
 moonraker_backup=${printer_config}/.moonraker_database_backup/moonraker_db_backup
+
+# switch to the klipper config directory
+cd ${printer_config}
+
+# loop through local repos looking to grab version information for backup
+# if the repo contains a .version file, grab the first line otherwise 
+# execute git commands to grab the last tag (version) & git revision number 
 
 for repo in "${gitrepos[@]}"; do
    trimedRepo="$(basename ${repo})"
-   if [ -d ${repo} ]; then
+
+   if [ -d ${repo} ]; then                    
       if [ -e ${repo}/.version ]; then
          rev="${trimedRepo} on commit: $(head -n 1 ${repo}/.version)"
       else
@@ -20,15 +33,14 @@ for repo in "${gitrepos[@]}"; do
    fi
 done
 
-cd ${printer_config}
-
+# trigger moonraker backup
 echo Backing moonraker database
 ~/moonraker/scripts/backup-database.sh -o ${moonraker_backup}
 
-echo Pushing updates
+# push updates to remote git repo
+echo Pushing GIT updates
 git pull -v "https://nigelpjames:$mygittoken@github.com/nigelpjames/voronred300.git"
 git add . -v
 current_date=$(date +"%Y-%m-%d %T")
-eval echo $gitrevs[@]
 git commit -m "Backup triggered on $current_date" "${gitrevs[@]}"
 git push "https://nigelpjames:$mygittoken@github.com/nigelpjames/voronred300.git"
