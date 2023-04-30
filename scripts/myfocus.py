d08dcf81fe8e7e8653842e8b79c4cc8961a07892
Nigel James <nigel@james.gen.nz>
2023-05-01 07:04:27 +1200

    Backup triggered on 2023-05-01 07:04:27
     "klipper on commit: v0.9.1-83308a1"
    
     "moonraker on commit: v0.8.0-6c7dfe5"
    
     "fluidd on commit: v1.23.5"
    
     "ERCF-Software-V3 on commit: v1.2.4-60711f7"
    
     "KlipperScreen on commit: v1.0.1-a6bf805"
    
     "klipper_z_calibration on commit: v1.0.0-73d62dc"

--- scripts/myfocus.py
+++ scripts/myfocus.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python3
-import sys
+import sys, argparse
 from ctypes import *
+
+parser = argparse.ArgumentParser()
+parser.add_argument('focus', type=int, nargs='?')
+args = parser.parse_args()
+
+if args.focus and args.focus >= 0 and args.focus <= 1024:
+  focus = args.focus
+else:
+  focus = 310
 
 arducam_vcm = CDLL('/home/pi/arducam/Motorized_Focus_Camera/python/lib/libarducam_vcm.so')
 
 arducam_vcm.vcm_init()
-focus=310
-#focus=200
 print("Setting arducam focus to ",focus)
-print("Arducam Response:",arducam_vcm.vcm_write(focus))
+try:
+  arducam_vcm.vcm_write(focus)
+except:
+  print("Error calling arducam library")
