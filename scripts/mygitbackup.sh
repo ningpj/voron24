cccac328e0d8de5f3f88cd8f25abd2c1f2d04ca4
Nigel James <nigel@james.gen.nz>
2023-05-14 16:16:24 +1200

    Backup triggered on 2023-05-14 16:16:24
     "klipper on commit: v0.9.1-83308a1"
    
     "moonraker on commit: v0.8.0-b21f177"
    
     "fluidd on commit: v1.23.5"
    
     "ERCF-Software-V3 on commit: v1.2.4-60711f7"
    
     "KlipperScreen on commit: v1.0.1-a6bf805"
    
     "klipper_z_calibration on commit: v1.0.1-7a09913"

--- scripts/mygitbackup.sh
+++ scripts/mygitbackup.sh
@@ -46,8 +46,8 @@ echo -e "\nBacking up moonraker database ..."
 # push updates to remote git repo
 echo -e "\nPushing GIT updates ..."
 
-git pull -v "https://nigelpjames:$mygittoken@github.com/nigelpjames/voronred300.git"
+git pull -v "https://nigelpjames:$mygittoken@github.com/nigelpjames/voron2.4.git"
 git add . -v
 current_date=$(date +"%Y-%m-%d %T")
 git commit -m "Backup triggered on $current_date" "${gitrevs[@]}"
-git push "https://nigelpjames:$mygittoken@github.com/nigelpjames/voronred300.git"
+git push "https://nigelpjames:$mygittoken@github.com/nigelpjames/voron2.4.git"
