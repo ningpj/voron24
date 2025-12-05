0b27d79441eaf330972c7efcd8b84f42f7467fdf
Nigel James <nigel@james.gen.nz>
2025-12-05 17:42:38 +1300

    Backup triggered on 2025-12-05 17:42:38
     "klipper on commit: v0.13.0-2e58023"
    
     "moonraker on commit: v0.9.3-058517b"
    
     "fluidd on commit: v1.35.1"
    
     "Happy-Hare on commit: v3.4.1-4873f9a"
    
     "KlipperScreen on commit: v3.0.0-6baed0f"
    
     "klipper_z_calibration on commit: v1.1.3-487056a"
    
     "klipper_tmc_autotune on commit: v0.2.0-8573b0d"
    
     "klippain_shaketune on commit: v5.2.1-786abbb"
    
     "crowsnest on commit: v4.1.17-9cc3d4a"

--- scripts/archive/mymakeklipper.sh
+++ scripts/archive/mymakeklipper.sh
@@ -3,22 +3,29 @@
 # exit on any error
 set -e
 
+# Set these based on your env
+rp_mcu=~/printer_data/config/scripts/config.rp_mcu
+spider_mcu=~/printer_data/config/scripts/config.spider_mcu
+sht36_mcu=~/printer_data/config/scripts/config.sht36v2
+#sht36_can=d10a59dcf8cc
+sht36_can=333f11f63709
+ercf_mcu=~/printer_data/config/scripts/config.ercfcanv1.1
+ercf_can=0cf6c395541c
+usb_mcu=/dev/serial/by-id/usb-Klipper_stm32f446xx_26001F000B50563046363120-if00
+
+
 # Targets
-ercf=0
 mcu=0
 rp=0
 flash=0
 sht36=0
-ercfcan=0
+ercf=0
 
 for ARG in $@; do
    case $ARG in
 
      "--sht36")
        sht36=1
-     ;;
-     "--ercfcan")
-       ercfcan=1
      ;;
      "--ercf")
        ercf=1
@@ -31,32 +38,24 @@ for ARG in $@; do
      ;;
      "--all")
        rp=1
-#       ercf=1
        mcu=1
-       ercfcan=1
-#       sht36=1
+       ercf=1
+       sht36=1
      ;;
      "--flash")
        flash=1
      ;;
      *)
-       echo "$0 Error. Invalid argument [$ARG]. Options include --all, --ercf, --mcu, --rp, --flash, --ercfcan, --sht36"
+       echo "$0 Error. Invalid argument [$ARG]. Options include --all, --mcu, --rp, --flash, --ercf, --sht36"
        exit 9
      ;;
    esac
 done
 
-if [ $ercf = 0 ] && [ $mcu = 0 ] && [ $rp = 0 ] && [ $ercfcan = 0 ] && [ $sht36 = 0 ]; then
+if [ $mcu = 0 ] && [ $rp = 0 ] && [ $ercf = 0 ] && [ $sht36 = 0 ]; then
   echo Nothing to do, exiting.
   exit 0
 fi
-
-rp_mcu=~/printer_data/config/scripts/config.rp_mcu
-spider_mcu=~/printer_data/config/scripts/config.spider_mcu
-seeedunio_mcu=~/printer_data/config/scripts/config.seeedunio_mcu
-sht36_mcu=~/printer_data/config/scripts/config.sht36v2
-ercfcan_mcu=~/printer_data/config/scripts/config.ercfcanv1.1
-usb_mcu=/dev/serial/by-id/usb-Klipper_stm32f446xx_26001F000B50563046363120-if00 
 
 if [ $flash = 1 ]; then
   read -p "*** WARNING *** MCU's will be updated and flashed!!! - Press [Enter] to continue, Ctrl-C to abort"
@@ -80,7 +79,7 @@ if [ $sht36 = 1 ]; then
 
   if [ $flash = 1 ]; then
     echo "Flashing Klipper to Fly SHT-36 V2 CAN Board"
-    python3 ~/katapult/scripts/flash_can.py -i can0 -f ~/klipper/out/klipper.bin -u d10a59dcf8cc
+    python3 ~/katapult/scripts/flash_can.py -i can0 -f ~/klipper/out/klipper.bin -u ${sht36_can}
     read -p "Fly SHT-36 V2 Can Board updated. Press [Enter] to continue or Ctrl-C to abort"
   else
     echo --flash not specified, flash step bypassed.
@@ -88,41 +87,20 @@ if [ $sht36 = 1 ]; then
   echo
 fi
 
-if [ $ercfcan = 1 ]; then
+if [ $ercf = 1 ]; then
   echo "Compiling Klipper for Fly ERCF V1.1 CAN Board"
 
-  echo Using make override [${ercfcan_mcu}]
-  make clean KCONFIG_CONFIG=${ercfcan_mcu}
-  make menuconfig KCONFIG_CONFIG=${ercfcan_mcu}
-  make KCONFIG_CONFIG=${ercfcan_mcu} -j4
+  echo Using make override [${ercf_mcu}]
+  make clean KCONFIG_CONFIG=${ercf_mcu}
+  make menuconfig KCONFIG_CONFIG=${ercf_mcu}
+  make KCONFIG_CONFIG=${ercf_mcu} -j4
 
   read -p "Review errors and press [Enter] to continue, Ctrl-C to abort"
 
   if [ $flash = 1 ]; then
     echo "Flashing Klipper to Fly ERCF V1.1 CAN Board"
-    #python3 ~/katapult/scripts/flash_can.py -i can0 -f ~/klipper/out/klipper.bin -u 16ebb6c69db4
-    #python3 ~/katapult/scripts/flash_can.py -i can0 -f ~/klipper/out/klipper.bin -u c0bfb3f3e9c9
-    python3 ~/katapult/scripts/flash_can.py -i can0 -f ~/klipper/out/klipper.bin -u 0cf6c395541c
+    python3 ~/katapult/scripts/flash_can.py -i can0 -f ~/klipper/out/klipper.bin -u ${ercf_can}
     read -p "Fly ERCF V1.1 CAN Board updated. Press [Enter] to continue or Ctrl-C to abort"
-  else
-    echo --flash not specified, flash step bypassed.
-  fi
-  echo
-fi
-
-if [ $ercf = 1 ]; then
-  echo "Compiling Klipper for Seeedunio MCU [ERCF]"
-
-  echo Using make override [${seeedunio_mcu}]
-  make clean KCONFIG_CONFIG=${seeedunio_mcu}
-  make menuconfig KCONFIG_CONFIG=${seeedunio_mcu}
-  make KCONFIG_CONFIG=${seeedunio_mcu}
-  read -p "Review errors and press [Enter] to continue, Ctrl-C to abort"
-
-  if [ $flash = 1 ]; then
-    echo "Flashing Klipper to Seeedunio MCU [ERCF]"
-    /usr/local/bin/bossac -i -d -p /dev/ttyACM1 -e -w -v -R --offset=0x2000 out/klipper.bin
-    read -p "Seeedunio MCU [ERCF] updated. Press [Enter] to continue or Ctrl-C to abort"
   else
     echo --flash not specified, flash step bypassed.
   fi
